ee6ff480841af612942d697b49be9aaf79f29a94
arsalan9997 <128313657+arsalan9997@users.noreply.github.com>
2026-09-08 14:47:04 +0530

    Create RockPaperScissorsApplication class

---
+++ src/main/java/com/devops/game/RockPaperScissorsApplication.java
@@ -0,0 +1,10 @@
+package com.devops.game;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RockPaperScissorsApplication.class, args);
+    }
+}
